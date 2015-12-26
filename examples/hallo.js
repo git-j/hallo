89501f9f63b85a120e1bfb9648dfc63dbff951bd
git-j <joerg+git-j@refeus.de>
2015-12-26 03:01:44 +0100

    avoid z3988 data be dropped by IDOM::clean

--- examples/hallo.js
+++ examples/hallo.js
@@ -1298,6 +1298,7 @@
         event.data.undoWaypointStart('paste');
         pdata = pdata.replace(/<script/g, '<xscript').replace(/<\/script/, '</xscript');
         jq_temp = jQuery('<div>' + pdata + '</div>');
+        jQuery('.Z3988', jq_temp).text('|');
         dom = new IDOM();
         dom.clean(jq_temp);
         html = jq_temp.html();
